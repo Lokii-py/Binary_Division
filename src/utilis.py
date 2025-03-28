0f1188348e5296cd8df37ecd50f00bf39e278075
Lokesh Das <ldd3g@umsystem.edu>
2025-03-28 14:37:01 -0500

    Updated the code for addBinary to work accurately needed for divison

---
+++ src/utilis.py
@@ -0,0 +1,39 @@
+def addBinary(BinNum1, BinNum2):
+    """
+    Takes two strings called BinNum1 and BinNum2
+    and converts them into decimal numbers first,
+    then adds them, and converts the result back to binary.
+    The result is trimmed to remove any overflow bit if it exceeds the original length.
+    """
+
+    #sum the binary num after converting the string into decimal 
+    BinarySum = bin(int(BinNum1, 2) + int(BinNum2, 2))
+
+    #trim the extra 0b
+    BinarySum = BinarySum[2:]
+
+    #find the maximum length from two binary number
+    max_len = max(len(BinNum1), len(BinNum2))
+
+    #Discard the overflow bit if the BinarySum has one
+    if len(BinarySum) != max_len:
+        x = len(BinarySum) - max_len
+        return BinarySum[x:]
+    
+    return BinarySum
+
+
+def FindTwosComplement():
+    return -1
+
+def subtractBinary():
+    return -1 
+
+def shiftLeft():
+    return -1
+
+def sequenceCounter():
+    return -1
+
+def checkOverflow():
+    return -1
