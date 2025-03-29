342061dc32700a928f25b72300aa8075f4a617c3
Lokesh Das <ldd3g@umsystem.edu>
2025-03-28 19:42:07 -0500

    Initated DivisionAlgo for the class of method of division

---
+++ src/DivisionAlgo.py
@@ -0,0 +1,11 @@
+import utils
+
+class Restoring:
+    def __init__(self, divided, divior):
+        return -1
+
+
+class NonRestoring:
+    def __init__(self, dividend, divisor):
+        return -1
+    
