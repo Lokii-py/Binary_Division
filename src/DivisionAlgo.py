c660f7de426f2802bb18616bb9673e2b859fab5b
Lokesh Das <lokeshdas@Lokeshs-MacBook-Air.local>
2025-03-29 19:44:22 -0500

    updated

--- src/DivisionAlgo.py
+++ src/DivisionAlgo.py
@@ -1,9 +1,24 @@
 import utils
 
 class Restoring:
-    def __init__(self, divided, divior):
-        return -1
+    e_bit = "0"
 
+    def __init__(self, dividend, divisor):
+        self.dividend = dividend
+        self.divisor = divisor
+        self.accumulator = "0" * (8-len(dividend))
+        self.iteration_count = 0
+
+    def run(self):
+        self.sequence_counter = True
+        print("Running the Restoring Division Methond...")
+        while self.sequence_counter:
+            checkOverflow()
+
+
+    
+    def displayResult(self):
+        return -1
 
 class NonRestoring:
     def __init__(self, dividend, divisor):
