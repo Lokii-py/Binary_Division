fab7c2e467b58de0f71d60335532be86f4acddc6
Lokesh Das <lokeshdas@Lokeshs-MacBook-Air.local>
2025-04-10 23:27:40 -0500

    Proper Handling of input and output

--- src/main.py
+++ src/main.py
@@ -1,26 +1,40 @@
-import DivisionAlgo
 from DivisionAlgo import Restoring, NonRestoring
 from utils import Hexadecimal
 
 def main():
     """
     This is the main function of the program. It takes user input for two binary signed numbers.
-    This program run for the signed binary numbers only.
+    This program run for the signed binary numbers only
     """
-    print("\nWelcome to the Binary Divison Program for the Signed number")
-    print("This program is only for signed binary numbers")
+    print("\nWelcome to the Binary Divison Program for the Signed number\n")
 
-    dividend = str(input("\nPlease enter the signed Binary Number as dividend: "))#Take input from user
-    divisor = str(input("Enter the signed Binary Number as divisor: "))
+    exit = False
+    while(not(exit)):
+        print("Binary Divsion Simulator")
+        print(" 1. Binary Division")
+        print(" 2. Quit\n")
 
-    print("\nRunning the Program......\n")
+        choice = input("Please enter your choice: ").strip()
 
-    D1 = Restoring(dividend, divisor)
-    D1.displayResult()
+        if choice == '2':
+            exit = True
+            break
+        elif choice != '1': #check for the valid choice
+            print("\nPLEASE ENTER A VALID CHOICE\n")
+            continue
+        else:
+            try:
+                dividend = input("\nPlease enter the signed Binary Number as dividend: ").strip() #Take input from user
+                divisor = input("Enter the signed Binary Number as divisor: ").strip()
 
-    print()
-    D2 = NonRestoring(dividend, divisor)
-    D2.displayResult()
+                D1 = Restoring(str(dividend), str(divisor))
+                D1.displayResult()
+                D2 = NonRestoring(str(dividend), str(divisor))
+                D2.displayResult()
+
+            except (ValueError, IndexError):
+                print("\nNo Reult for the invalid inputs. Please enter only a valid binary number\n")
+                continue
 
 if __name__ == "__main__":
     main()
